76e52b51012a554d05faa2283b686176c9cfb75a
Claire Chang <c89650508@hotmail.com>
2020-10-24 09:05:17 -0500

    update homepage

---
+++ src/components/News.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './News.css';
+
+const News = ({
+    date, content
+}) => {
+    return (
+        <div className="news-containter">
+            <div>
+            <p><span>{date}</span><span> {content}</span></p>
+            </div>
+        </div>
+    )
+}
+
+export default News
--- src/pages/issuepage.js
+++ src/pages/issuepage.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './issuepage.css';
 import Header from '../components/Header';
-import Navbar from '../components/Navbar';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function issuepage() {
   return (
     <div>
-      <Navbar />
       <Header title="ISSUE SPEAKER" />
     </div>
   );
