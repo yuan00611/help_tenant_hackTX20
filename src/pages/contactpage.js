58542478d7f784e2cd38c0d40bb953b2a082af2b
陳德元 <r06521608@ntu.edu.tw>
2020-10-23 23:53:43 -0500

    fix header and navbar

--- src/pages/contactpage.js
+++ src/pages/contactpage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './contactpage.css';
 import Header from '../components/Header';
-// import Navbar from './components/Navbar';
+import Navbar from '../components/Navbar';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function contactpage() {
   return (
     <div>
+      <Navbar />
       <Header title="CONTACT" />
     </div>
   );
