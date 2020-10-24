9e4e107df0dff9bba7536386be306838a184a5ed
陳德元 <r06521608@ntu.edu.tw>
2020-10-23 19:15:27 -0500

    create five different pages and works well for sidebar to switch pages

---
+++ src/pages/filepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './filepage.css';
+// import Navbar from './components/Navbar';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function filepage() {
+  return (
+    <div>
+      <h1>FILE ARCHIVE</h1>
+    </div>
+  );
+}
+
+export default filepage;
