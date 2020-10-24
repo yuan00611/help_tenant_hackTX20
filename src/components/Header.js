0cbb75fc1dbced143afa018a7e05c187babf224f
Claire Chang <c89650508@hotmail.com>
2020-10-24 13:53:40 -0500

    update issue page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Header.css';
 import { AiOutlineArrowLeft } from "react-icons/ai";
+import { IconContext } from 'react-icons';
 import { Link } from 'react-router-dom';
+
 // import Navbar from './components/Navbar';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function Header(props) {
   return (
     <div>
-      <h1> {props.title} </h1>
-      <Link to='/'> 
-         <AiOutlineArrowLeft className="arrow" />
+      <IconContext.Provider value={{size: '2rem'}}>
+      <Link to='/home'> 
+        <h2><AiOutlineArrowLeft className="arrow" /> {props.title}</h2>
       </Link>
+      </IconContext.Provider>
     </div>
   );
 }
