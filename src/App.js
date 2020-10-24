eb2cadf7103ac7a36c9e9bde49697acf99aef2a8
陳德元 <r06521608@ntu.edu.tw>
2020-10-24 14:08:01 -0500

    chat part update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import homepage from './pages/homepage.js';
 import issuepage from './pages/issuepage.js';
-import chatpage from './pages/chatpage.js';
+import Chatpage from './pages/chatpage.js';
 import filepage from './pages/filepage.js';
 import contactpage from './pages/contactpage.js';
 
@@ -16,7 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={homepage} />
         <Route  path='/issue' component={issuepage} />
-        <Route  path='/chat' component={chatpage} />
+        <Route  path='/chat' component={Chatpage} />
         <Route  path='/file' component={filepage} />
         <Route  path='/contact' component={contactpage} />
       </Switch>
