54ce63460e97babd3f5c664c907dd3b8c459d1cc
drinkstyle <c89650508@hotmail.com>
2020-10-24 16:40:13 -0500

    update file archive page (#8)
    * create testpage
    
    * update file archive page

--- src/pages/filepage.js
+++ src/pages/filepage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './filepage.css';
 import Header from '../components/Header';
 import Button from '../components/Button';
+import File from '../components/File'
+
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function filepage() {
@@ -15,6 +17,28 @@ function filepage() {
       </div>
       <div className="section">
         <h3>All files</h3>
+        <div className="file-table">
+          <File
+          name="Name"
+          date="Date Created"
+          size="Size"
+          url="" />
+          <File
+          name="Toilet repair request.pdf"
+          date="Oct 19, 2020"
+          size="1.2MB"
+          url="/" />
+          <File
+          name="Broken window.jpg"
+          date="Oct 08, 2020"
+          size="465kB"
+          url="/" />
+          <File
+          name="Oct utility bill.pdf"
+          date="Oct 19, 2020"
+          size="1.5MB"
+          url="/" />
+        </div>
       </div>
 
       <div class="button">
