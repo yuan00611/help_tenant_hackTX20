25463fb9cf53132104e64e598c6de3d06db3c9d6
Claire Chang <c89650508@hotmail.com>
2020-10-24 11:47:11 -0500

    updat navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,68 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
 import { IconContext } from 'react-icons';
-import { Link } from 'react-router-dom';
+import { FaQuestion, FaFolder, FaPhoneAlt, FaHome } from 'react-icons/fa';
+import { BsFillChatDotsFill } from 'react-icons/bs';
 import './Navbar.css'
 
-function Navbar() {
+const Navbar = ({currentRoute}) => {
+    
     return (
         <>
         <IconContext.Provider value={{color: '#000'}}>
             <div className="navbar">
                 <div className="navbar-container">
-                    <Link to='/' className="navbar-logo">
-                        TenantHelp
+                    <Link to='/home'>
+                        <div className="navbar-logo">
+                        <span className="primary-color">Tenant</span><span className="secondary-color">Circle</span>
+                        </div>
                     </Link>
-                    <ul>
-                        <li className="nav-item">
-                            <Link to="/" className="nav-links">
-                                Home
-                            </Link>
+                    <ul className="section">
+                        <li>
+                            <NavLink to="/home" activeStyle={{
+                                borderLeft: "0.4rem solid #9b51e0",
+                                backgroundColor: "#ffffff", 
+                                color: "#000000"
+                            }} className="nav-item">
+                                    <FaHome /><span className="nav-title">Home</span>
+                            </NavLink>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/issue" className="nav-links">
-                                Issue Speaker
-                            </Link>
+                        <li>
+                            <NavLink to="/issue" activeStyle={{
+                                borderLeft: "0.4rem solid #9b51e0",
+                                backgroundColor: "#ffffff", 
+                                color: "#000000"
+                            }} className="nav-item">
+                                <FaQuestion /><span className="nav-title">Issue Speaker</span>
+                            </NavLink>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/chat" className="nav-links">
-                                Tenant Chat
-                            </Link>
+                        <li>
+                            <NavLink to="/chat" activeStyle={{
+                                borderLeft: "0.4rem solid #9b51e0",
+                                backgroundColor: "#ffffff", 
+                                color: "#000000"
+                            }} className="nav-item">
+                                <BsFillChatDotsFill /><span className="nav-title">Tenant Chat</span>
+                            </NavLink>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/file" className="nav-links">
-                                File Archive
-                            </Link>
+                        <li>
+                            <NavLink to="/file" activeStyle={{
+                                borderLeft: "0.4rem solid #9b51e0",
+                                backgroundColor: "#ffffff", 
+                                color: "#000000"
+                            }} className="nav-item">
+                                <FaFolder /><span className="nav-title">File Archive</span>
+                            </NavLink>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/contact" className="nav-links">
-                                Contact Landlord
-                            </Link>
+                        <li>
+                            <NavLink to="/contact" activeStyle={{
+                                borderLeft: "0.4rem solid #9b51e0",
+                                backgroundColor: "#ffffff", 
+                                color: "#000000"
+                            }} className="nav-item">
+                                <p><FaPhoneAlt /><span className="nav-title">Contact Landlord</span></p>
+                            </NavLink>
                         </li>
                     </ul>
                 </div>
