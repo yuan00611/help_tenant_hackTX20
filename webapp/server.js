8ee53cdf7dd6176fff92cd3a079d4d8e3b0ddc00
Darren Hou <houdarren@users.noreply.github.com>
2020-10-25 00:14:11 -0500

    Modify webserver (#13)

--- webapp/server.js
+++ webapp/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const bodyParser = require("body-parser");
 
 //setup app & its routes
 const app = express();
-app.use(cors());
+//app.use(cors());
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+//
+
+
 const routes = require('./routes/index.route');
 app.use(routes);
 
