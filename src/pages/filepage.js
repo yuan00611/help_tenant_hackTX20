d096ec428dfcb58582ab07e996a85d377c11495b
Claire Chang <c89650508@hotmail.com>
2020-10-25 12:03:49 -0500

    make buttons work

--- src/pages/filepage.js
+++ src/pages/filepage.js
@@ -3,20 +3,48 @@ import './filepage.css';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import { Files } from '../components/Files';
+// import File from '../components/File';
 
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-function filepage() {
+const File = () => {
   return (
-    <div className="container">
-      <Header title="FILE ARCHIVE" />
-      <Files />
-
-      <div className="button">
-      <Button content="+ New File" />
+    <div className="file-container">
+      <div className="file-row">
+          <div className="file">
+              <p><a href="/">Oct utility bill.pdf</a></p>
+              <p>10/25/2020</p>
+              <p>455kB</p>
+          </div>
       </div>
     </div>
-  );
+  )
+}
+
+class filepage extends React.Component{
+  state = {
+    files: []
+  }
+
+  addFile = () => {
+    this.setState({
+      files: [...this.state.files, <File />]
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+      <Header title="FILE ARCHIVE" />
+      <Files />
+      {this.state.files}
+      
+
+      <div className="button" onClick={this.addFile}>
+      <Button content="+ New File" />
+      
+      </div>
+      </div>
+    );
+  }
 }
 
 export default filepage;
