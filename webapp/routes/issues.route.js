a8b9d9308bb65b9eb1cd4804e611a60fdfb6fc7f
Darren Hou <houdarren@users.noreply.github.com>
2020-10-25 00:11:47 -0500

    Modify webserver

--- webapp/routes/issues.route.js
+++ webapp/routes/issues.route.js
@@ -1,27 +1,53 @@
 const express = require('express');
+//const bodyParser = require("body-parser");
+
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/all')
+
+const auth = require('../auth');
+
+const mysql = require('mysql');
+
+let connection = mysql.createConnection(auth);
+
+router.route('/solved')
     .get(function (req, res) {
-    	res.send(
-    		[
-    			{
-    				"timestamp": 100,
-    				"message": "test",
-    				"user": "darren",
-    				"count": 10,
-    				"solved": "T"
-    			},
-    			{
-    				"timestamp": 101,
-    				"message": "test2",
-    				"user": "darren",
-    				"count": 1,
-    				"solved": "T"
-    			}
-    		]
-    	);
-    });
+
+    	//var issues = ""
+
+    	connection.query("select * from issues where solved=\"T\" order by timestamp DESC;",
+    		function(error, results, fields) {
+		        if (error) {
+		        	throw error;
+		        }
+		        res.send(results);
+      		});
+    })
+
+router.route('/unsolved')
+    .get(function (req, res) {
+
+    	connection.query("select * from issues where solved=\"F\" order by timestamp DESC;",
+    		function(error, results, fields) {
+		        if (error) {
+		        	throw error;
+		        }
+		        res.send(results);
+      		});
+    })
+
+router.route('/new')
+    .post(function (req, res) {
+    	var body = req.body;
+    	var query = `INSERT INTO issues (message, timestamp, count, solved) VALUES (${body.message}, ${new Date().getTime()}, 1, "F");`
+    	var response = connection.query(query);
+    	res.send("Success!");
+    })
+
+router.route('/plus')
+	.post(function (req, res) {
+		var body = req.body;
+	})
 
 module.exports = router;
